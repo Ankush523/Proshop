46609fd9171a4c1bfa97f5e4ad9c685a5962ee54
Ankush <ankush.ad050203@gmai.com>
2022-05-14 17:26:10 +0530

    Individual_Product_viewing

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { Row,Col,Image,ListGroup,Card,Button,  } from 'react-bootstrap'
+import { Link, useParams } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
 
-const ProductScreen = ({match}) => {
-  const product = products.find((p) => p._id === match.params.id)
+const ProductScreen = () => {
+  const {id} = useParams();
+  const product = products.find((p) => p._id === (id))
   return (
     < >
        <Link className='btn btn-light my-3' to='/'>
@@ -18,9 +19,9 @@ const ProductScreen = ({match}) => {
          <Col md={3}>
            <ListGroup variant='flush'>
              <ListGroup.Item>
-               <h2>
+               <h3>
                  {product.name}
-               </h2>
+               </h3>
              </ListGroup.Item>
              <ListGroup.Item>
                <Rating 
